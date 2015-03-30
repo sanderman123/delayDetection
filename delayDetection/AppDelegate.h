6fb4a70fb24aa172135dd74afcfd2c52f34d66f6
Sander Valstar <sanderman123@hotmail.com>
2015-03-30 11:00:28 +0800

    Initial Commit

---
+++ delayDetection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  delayDetection
+//
+//  Created by Sander Valstar on 3/30/15.
+//  Copyright (c) 2015 Sander Valstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ delayDetection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delayDetection
+//
+//  Created by Sander Valstar on 3/30/15.
+//  Copyright (c) 2015 Sander Valstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
